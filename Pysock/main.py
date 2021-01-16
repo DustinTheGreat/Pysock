2aff3623c0db4e6dca60d9aa6c1fcfdcb48f69f9
dustinthegreat <dustinroberts96@gmail.com>
2021-01-15 22:36:08 -0500

    added main function to call the classes

--- Pysock/main.py
+++ Pysock/main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+import asyncio
+import pathlib
+import ssl
+import websockets
+
+
+async def main(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f" {name}!"
+
+    await websocket.send(payload)
+    print(f"> {paylaod}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+	main()
