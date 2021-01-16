8267a85e7a6fbc43d24475f75dbceb0f4d3d6e3f
dustinthegreat <dustinroberts96@gmail.com>
2021-01-16 00:59:44 -0500

    Just making connection, thinking I should check for SSL first

--- Pysock/main.py
+++ Pysock/main.py
@@ -5,20 +5,57 @@ import ssl
 import websockets
 
 
-async def main(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
 
-    greeting = f" {name}!"
 
-    await websocket.send(payload)
-    print(f"> {paylaod}")
 
-start_server = websockets.serve(hello, "localhost", 8765)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+
+# async def consumer_handler(websocket, path):
+#     async for message in websocket:
+#         await consumer(message)
+
+
+
+
+
+
+# async def producer_handler(websocket, path):
+#     while True:
+#         message = await producer()
+#         await websocket.send(message)
+
+
+
+
+
+
+
+
+async def connection():
+	uri = "ws://localhost:5678"
+	async with websockets.connect(uri) as websocket:
+		##add the dictonary that you want to test against
+		name = input("Im in")
+
+		await websocket.send(name)
+		print(f"> {name}")
+
+		greeting = await websocket.recv()
+		print(f"< {greeting}")
+
+
+
+	
+async def main():
+	print("Starting...")
+	await connection()
+	print("Finishing")
 
 
 if __name__ == "__main__":
-	main()
+	# import cli runner
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(main())
+	# ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+	# localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+	# ssl_context.load_verify_locations(localhost_pem)	
